899bdeb325c90899fe84573c2e1ba8d7c0f170d6
binbin <774663576@qq.com>
2015-01-22 17:31:28 +0800

    checkout mobile

--- src/com/meitu/controller/UserController.java
+++ src/com/meitu/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.meitu.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.meitu.bean.User;
 import com.meitu.dao.UserDao;
+import com.meitu.enums.ErrorEnum;
+import com.meitu.utils.JsonUtil;
 
 @Controller
 public class UserController {
@@ -24,6 +29,22 @@ public class UserController {
 		user.setUser_phone(request.getParameter("user_phone"));
 		dao.add(user);
 		return user.toString();
+	}
+
+	@ResponseBody
+	@RequestMapping("/checkoutMobilNum")
+	public String checkoutMobile(HttpServletRequest request) {
+		String mobile_num = request.getParameter("user_phone");
+		boolean isExist = dao.verifyCellphone(mobile_num);
+		Map<String, Object> params = new HashMap<String, Object>();
+		if (isExist) {
+			params.put("err", ErrorEnum.USER_ALREADY_EXIST.name());
+			params.put("rt", 0);
+		} else {
+			params.put("rt", 1);
+		}
+		return JsonUtil.toJsonString(params);
+
 	}
 
 	public UserDao getDao() {
---
+++ src/com/meitu/enums/ErrorEnum.java
@@ -0,0 +1,5 @@
+package com.meitu.enums;
+
+public enum ErrorEnum {
+	WRONG_PASSWORD, NOT_EXIST_USER, USER_ALREADY_EXIST, INVALID, SMS_CODE_ERR,
+}
